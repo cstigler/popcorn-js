38d21f45cd33eb024803ab97bbfe0eb141394e47
Brian Chirls <brian@chirls.com>
2012-10-05 08:21:58 -0400

    Youtube: Buffered feature #1244

--- wrappers/youtube/popcorn.HTMLYouTubeVideoElement.js
+++ wrappers/youtube/popcorn.HTMLYouTubeVideoElement.js
@@ -86,6 +86,9 @@
       playerReady = false,
       player,
       playerReadyCallbacks = [],
+      playerState = -1,
+      bufferedInterval,
+      lastLoadedFraction = 0,
       currentTimeInterval,
       lastCurrentTime = 0,
       seekTarget = -1,
@@ -191,6 +194,13 @@
           // XXX: this should really live in cued below, but doesn't work.
           impl.readyState = self.HAVE_METADATA;
           self.dispatchEvent( "loadedmetadata" );
+          if (!playerReady) {
+            addPlayerReadyCallback( function() {
+              bufferedInterval = setInterval( monitorBuffered, 50 );
+            });
+          } else {
+            bufferedInterval = setInterval( monitorBuffered, 50 );
+          }
 
           self.dispatchEvent( "loadeddata" );
 
@@ -240,6 +250,11 @@
           // XXX: cued doesn't seem to fire reliably, bug in youtube api?
           break;
       }
+      if (event.data !== YT.PlayerState.BUFFERING && playerState === YT.PlayerState.BUFFERING) {
+        onProgress();
+      }
+
+      playerState = event.data;
     }
 
     function destroyPlayer() {
@@ -247,6 +262,7 @@
         return;
       }
       clearInterval( currentTimeInterval );
+      clearInterval( bufferedInterval );
       player.stopVideo();
       player.clearVideo();
 
@@ -362,6 +378,20 @@
       lastCurrentTime = impl.currentTime;
     }
 
+    function monitorBuffered() {
+      var fraction = player.getVideoLoadedFraction();
+
+      if ( lastLoadedFraction !== fraction ) {
+        lastLoadedFraction = fraction;
+
+        onProgress();
+
+        if (fraction >= 1) {
+          clearInterval( bufferedInterval );
+        }
+      }
+    }
+
     function getCurrentTime() {
       if( !playerReady ) {
         return 0;
@@ -432,6 +462,10 @@
         }
         self.dispatchEvent( "playing" );
       }
+    }
+
+    function onProgress() {
+      self.dispatchEvent( "progress" );
     }
 
     self.play = function() {
@@ -623,6 +657,45 @@
       error: {
         get: function() {
           return impl.error;
+        }
+      },
+
+      buffered: {
+        get: function () {
+          var timeRanges = {
+            start: function( index ) {
+              if (index === 0) {
+                return 0;
+              }
+
+              //throw fake DOMException/INDEX_SIZE_ERR
+              throw "INDEX_SIZE_ERR: DOM Exception 1";
+            },
+            end: function( index ) {
+              var duration;
+              if (index === 0) {
+                duration = getDuration();
+                if (!duration) {
+                  return 0;
+                }
+
+                return duration * player.getVideoLoadedFraction();
+              }
+
+              //throw fake DOMException/INDEX_SIZE_ERR
+              throw "INDEX_SIZE_ERR: DOM Exception 1";
+            }
+          };
+
+          Object.defineProperties( timeRanges, {
+            length: {
+              get: function() {
+                return 1;
+              }
+            }
+          });
+
+          return timeRanges;
         }
       }
     });
